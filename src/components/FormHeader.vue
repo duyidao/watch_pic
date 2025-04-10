a931b087ca9138c4c505edf14b8289842dd3fab6
v_duyilin <v_duyilin@baidu.com>
2025-04-11 00:43:41 +0800

    prod: 修改样式

--- src/components/FormHeader.vue
+++ src/components/FormHeader.vue
@@ -60,39 +60,39 @@ const changeCkeckFn = (e: Event, type: 'choseDirectory' | 'choseIpDirectory') =>
     </div>
     <div class="header-tool">
       <button @click="openDirectory">打开文件夹</button>
-      <input
-        :value="findText"
+      <input :value="findText"
         type="text"
         placeholder="请输入精确搜索的关键词"
-        @change="changeTextFn"
-      />
-      <select
-        :value="imgType"
+        @change="changeTextFn" />
+      <select :value="imgType"
         name="imgType"
         id=""
-        @change="changeImgType"
-      >
+        @change="changeImgType">
         <option value="">全部</option>
-        <option v-for="item in imgTypeOptions" :key="item" :value="item">{{ item }}</option>
+        <option v-for="item in imgTypeOptions"
+          :key="item"
+          :value="item">{{ item }}</option>
       </select>
+      <button>重新选择要保存的文件夹</button>
+      <button @click="downloadImgFn">下载图片</button>
     </div>
     <div class="header-download">
       <label for="folder">
-        <input
-          :value="choseDirectory"
-          type="checkbox" name="folder" id="folder"
+        <input :value="choseDirectory"
+          type="checkbox"
+          name="folder"
+          id="folder"
           @change="changeCkeckFn($event, 'choseDirectory')" />
         是否下载到指定文件夹
       </label>
       <label for="ip">
-        <input
-          :value="choseIpDirectory"
-          type="checkbox" name="ip" id="ip"
+        <input :value="choseIpDirectory"
+          type="checkbox"
+          name="ip"
+          id="ip"
           @change="changeCkeckFn($event, 'choseIpDirectory')" />
         是否携带ip目录
       </label>
-      <button>重新选择要保存的文件夹</button>
-      <button @click="downloadImgFn">下载图片</button>
     </div>
   </header>
 </template>
@@ -100,9 +100,64 @@ const changeCkeckFn = (e: Event, type: 'choseDirectory' | 'choseIpDirectory') =>
 <style lang="less" scoped>
 header {
   padding: 20px;
-  &-tool {
+
+  .header-total {
     display: flex;
     align-items: center;
+    font-size: 20px;
+
+    div {
+      margin: 0 15px;
+    }
+  }
+
+  .header-tool {
+    display: flex;
+    align-items: center;
+    margin: 15px 0;
+
+    button {
+      height: 30px;
+      border-radius: 8px;
+      margin-right: 20px;
+      padding: 0 20px;
+    }
+
+    input {
+      min-width: 180px;
+      height: 30px;
+      margin-right: 20px;
+      padding-left: 12px;
+      border: 1px solid #ccc;
+    }
+
+    select {
+      min-width: 180px;
+      height: 30px;
+      margin-right: 20px;
+      padding-left: 12px;
+      border: 1px solid #ccc;
+
+      &:focus-visible {
+        outline: none;
+      }
+    }
+  }
+
+  .header-download {
+    display: flex;
+    align-items: center;
+
+    label {
+      margin-right: 20px;
+      font-size: 14px;
+      display: flex;
+      align-items: center;
+
+      input {
+        margin-right: 8px;
+      }
+    }
   }
 }
 </style>
--- src/components/ShowImg.vue
+++ src/components/ShowImg.vue
@@ -5,11 +5,20 @@ defineProps<{
 </script>
 
 <template>
-  <div>
+  <div class="show-img">
     <img :src="src" alt="">
   </div>
 </template>
 
-<style scoped>
+<style lang="less" scoped>
+.show-img {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: calc(100vh - 135px);
 
+  img {
+    height: 100%;
+  }
+}
 </style>
